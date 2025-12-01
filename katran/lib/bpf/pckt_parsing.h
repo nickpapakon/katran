e224e4a049f1624b7353b1126d97310d3eaa6bca
ntua-el20069 <el20069@mail.ntua.gr>
2025-12-01 22:46:21 +0200

    [mqtt] bpf_printk accepts up to 3 additional params

--- katran/lib/bpf/pckt_parsing.h
+++ katran/lib/bpf/pckt_parsing.h
@@ -37,7 +37,7 @@
 
 #include "katran/lib/bpf/balancer_consts.h"
 #include "katran/lib/bpf/balancer_helpers.h"
-#include "mqtt.h"
+#include "katran/lib/bpf/mqtt.h"
 
 struct quic_long_header {
   __u8 flags;
@@ -134,10 +134,11 @@ __attribute__((__always_inline__)) static inline bool parse_tcp(
   }
 
   if(DIPLOMA_DEBUG){
-    const char *delimeter = "\n\n*************************\n";
+    const char *delimiter = "\n\n*************************\n";
 
+    bpf_printk("%s"); 
     bpf_printk("Parsing TCP packet: src port: %d, dst port: %d\n",
-      bpf_ntohs(tcp->source), bpf_ntohs(tcp->dest)
+      delimiter, bpf_ntohs(tcp->source), bpf_ntohs(tcp->dest)
     );
     
     // Parse MQTT if dst port is MQTT_PORT
@@ -146,8 +147,7 @@ __attribute__((__always_inline__)) static inline bool parse_tcp(
       if((void *)(mqtt_h + 1) > data_end){
           bpf_printk("Does not contain all the required MQTT hdr bytes");
       } else {
-        bpf_printk("%s (MQTT) \n msg_type = 0x%x, flags = 0x%x, remaining_len = 0x%x \n",
-            delimeter,
+        bpf_printk("(MQTT) \n msg_type = 0x%x, flags = 0x%x, remaining_len = 0x%x \n",
             mqtt_h->msg_type,
             mqtt_h->flags,
             mqtt_h->remaining_len
